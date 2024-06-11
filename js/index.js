3cad757daa6fdfa11dc13e672e863b991bb61d27
Noah Emmenegger <noahemmenegger@gmx.ch>
2024-06-11 13:54:08 +0200

    shrink js file

--- js/index.js
+++ js/index.js
@@ -12,49 +12,33 @@ let addresses = [
 
 function generateAddressInputs(addresses) {
     const addressInputs = addresses.map((address, index) => {
-        return `
-            <h3 style="${
-                index === 0 ? 'color: green;' : index === addresses.length - 1 ? 'color: red;' : ''
-            } border-top: 1px solid black;">
-                ${
-                    index === 0
-                        ? 'Startadresse'
-                        : index === addresses.length - 1
-                        ? 'Zieladresse'
-                        : `Address ${index + 1}`
-                } 
-                <button onclick="deleteAddress(${index})" style="background-color: var(--primary-color); color: white;">Delete</button>
-            </h3>
-           
-            <div class="address-input">
-                ${getInput(index, 'street', address.street, validate(address.street, ['required', 'min:2']))}
-                ${getInput(index, 'city', address.city, validate(address.city, ['required', 'min:2']))}
-                ${getInput(index, 'zip', address.zip, validate(address.zip, ['required', 'number', 'min:4']))}
-            </div>
-        `;
+        return `<h3 style="${
+            index === 0 ? 'color: green;' : index === addresses.length - 1 ? 'color: red;' : ''
+        } border-top: 1px solid black;">${
+            index === 0 ? 'Startadresse' : index === addresses.length - 1 ? 'Zieladresse' : `Address ${index + 1}`
+        } <button onclick="deleteAddress(${index})" style="background-color: var(--primary-color); color: white;">Delete</button></h3><div class="address-input">${getInput(
+            index,
+            'street',
+            address.street,
+            validate(address.street, ['required', 'min:2'])
+        )}${getInput(index, 'city', address.city, validate(address.city, ['required', 'min:2']))}${getInput(
+            index,
+            'zip',
+            address.zip,
+            validate(address.zip, ['required', 'number', 'min:4'])
+        )}</div>`;
     });
 
     document.getElementById('addresses').innerHTML = addressInputs.join('');
 }
 
 function getInput(index, key, value, error = '') {
-    return `
-        <div>
-            <label for="${key}-${index}">
-                ${key.charAt(0).toUpperCase() + key.slice(1)}:
-                ${error ? `<span style="color: red;">${error}</span>` : ''}
-            </label>
-            <input
-                type="text"
-                id="${key}-${index}"
-                value="${value}"
-                onchange="updateAddress(${index}, '${key}', this.value)"
-            />
-        </div>`;
+    return `<div><label for="${key}-${index}">${key.charAt(0).toUpperCase() + key.slice(1)}:${
+        error ? `<span style="color: red;">${error}</span>` : ''
+    }</label><input type="text" id="${key}-${index}" value="${value}" onchange="updateAddress(${index}, '${key}', this.value)" /></div>`;
 }
 
 function addAddress() {
-    // push second last
     addresses.splice(addresses.length - 1, 0, { street: '', city: '', zip: '' });
     generateAddressInputs(addresses);
 }
@@ -86,43 +70,27 @@ function parseJSON(text) {
 }
 
 function showRoute(response) {
-    // display route on map
-    document.getElementById('route').innerHTML = `
-       <div>
-            <h3>${response.message}</h3>
-            <p>Wir haben aus den eingegebenen Adressen die folgende Route berechnet. Es wurde die schnellste Route gewählt. Sie sollen für möglichst schnelle Ankunft die Route befolgen.</p>
-            <p>Benötigte Zeit: ${(response.summary.duration / 60).toFixed(2)} Minuten </p>
-            <p>Benutzte Berechnung für Fahrrad: ${response['vehicle_bike_count']}</p>
-            <p>Benutzte Berechnung für Auto: ${response['vehicle_car_count']}</p>
-            <p>Benutzte Berechnung für Fussgänger: ${response['vehicle_foot_count']}</p>
-            ${response.routes[0].steps
-                .map((step, index) => {
-                    return `
-                    <div>
-                        <p>${index + 1}. ${step.description}</p>
-                    </div>
-                `;
-                })
-                .join('')}
-        </div>
-    `;
+    document.getElementById('route').innerHTML = `<div><h3>${
+        response.message
+    }</h3><p>Wir haben aus den eingegebenen Adressen die folgende Route berechnet. Es wurde die schnellste Route gewählt. Sie sollen für möglichst schnelle Ankunft die Route befolgen.</p><p>Benötigte Zeit: ${(
+        response.summary.duration / 60
+    ).toFixed(2)} Minuten </p><p>Benutzte Berechnung für Fahrrad: ${
+        response['vehicle_bike_count']
+    }</p><p>Benutzte Berechnung für Auto: ${response['vehicle_car_count']}</p><p>Benutzte Berechnung für Fussgänger: ${
+        response['vehicle_foot_count']
+    }</p>${response.routes[0].steps
+        .map((step, index) => {
+            return `<div><p>${index + 1}. ${step.description}</p></div>`;
+        })
+        .join('')}</div>`;
 }
 
 function showErrorMessage(message) {
-    document.getElementById('route').innerHTML = `
-        <div class="w3-panel w3-red">
-            <h3>Error</h3>
-            <p>${message}</p>
-        </div>
-    `;
+    document.getElementById('route').innerHTML = `<div class="w3-panel w3-red"><h3>Error</h3><p>${message}</p></div>`;
 }
 
 function showLoading() {
-    document.getElementById('route').innerHTML = `
-        <div class="w3-panel w3-blue">
-            <h3>Loading...</h3>
-        </div>
-    `;
+    document.getElementById('route').innerHTML = `<div class="w3-panel w3-blue"><h3>Loading...</h3></div>`;
 }
 
 function validateRequest(addresses, date, time, vehicle) {
@@ -164,7 +132,6 @@ function calculateRoute() {
         return;
     }
 
-    // create request JSON document
     let body = { addresses, date, time, vehicle };
 
     let xhr = new XMLHttpRequest();
@@ -172,7 +139,6 @@ function calculateRoute() {
     xhr.ontimeout = () => showErrorMessage('The remote system did not respond in time. Please try again later.');
     xhr.onload = function () {
         let response = parseJSON(xhr.responseText);
-        // verify http code and JSON format
         if (xhr.status == 200 && response != null) {
             showRoute(response);
             const locations = response.routes[0].steps.map((step) => {
@@ -184,7 +150,6 @@ function calculateRoute() {
                 routeWhileDragging: false,
             }).addTo(map);
 
-            // Erstellen eines Polylinienpfads durch die Standorte
             const polyline = L.polyline(locations, { color: 'blue' }).addTo(map);
             map.fitBounds(polyline.getBounds());
         } else {
@@ -195,7 +160,6 @@ function calculateRoute() {
         }
     };
 
-    // send document to backend using a POST request
     xhr.open('POST', 'backend.php', true);
     xhr.send(JSON.stringify(body));
 }
@@ -207,34 +171,28 @@ function draw() {
 
     ctx.fillStyle = '#ff5e00';
     ctx.strokeStyle = '#ff5e00';
-
-    // Draws a simple package symbol
-    ctx.fillRect(0, 50, 100, 70); // Outer rectangle
+    ctx.fillRect(0, 50, 100, 70);
     ctx.beginPath();
     ctx.moveTo(0, 85);
-    ctx.lineTo(200, 85); // Middle line
+    ctx.lineTo(200, 85);
     ctx.stroke();
-
-    // Draws a simple route symbol
     ctx.beginPath();
-    ctx.arc(200, 85, 20, 0, Math.PI * 2, true); // Start point
+    ctx.arc(200, 85, 20, 0, Math.PI * 2, true);
     ctx.fill();
     ctx.beginPath();
-    ctx.arc(300, 85, 20, 0, Math.PI * 2, true); // End point
+    ctx.arc(300, 85, 20, 0, Math.PI * 2, true);
     ctx.fill();
     ctx.beginPath();
     ctx.moveTo(220, 85);
-    ctx.lineTo(280, 85); // Route line
+    ctx.lineTo(280, 85);
     ctx.stroke();
 }
 
 generateAddressInputs(addresses);
 
-const map = L.map('map').setView([47.195, 8.525], 13); // Zentrum der Karte auf Zug, Schweiz
+const map = L.map('map').setView([47.195, 8.525], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
